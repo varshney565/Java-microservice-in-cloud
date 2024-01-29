e4d5ae30472db2ba93941a307fdb4d378b5522f8
shivam <uietcse1923shivamvarshney@kuk.ac.in>
2024-01-29 20:06:55 +0530

    initial commit

---
+++ C4.java
@@ -0,0 +1,21 @@
+class C4 {
+    public static void main(String[] args) {
+        
+        /**
+         * 	byte (number, 1 byte)
+         *  short (number, 2 bytes)
+         *  int (number, 4 bytes)
+         *  long (number, 8 bytes)
+         *  float (float number, 4 bytes)
+         *  double (float number, 8 bytes)
+         *  char (a character, 2 bytes)
+         *  boolean (true or false, 1 byte)
+         * */
+         
+         byte a = 12;
+         System.out.
+		
+         
+    }
+}
+	
